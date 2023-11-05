b790512641b1a9e838cef93793f1af28ad4e5d22
anil90826 <anilkumar.90826@gmail.com>
2023-11-05 15:55:13 -0500

    intial commit

---
+++ src/main/java/com/roadhousebre/RoadhouseBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.roadhousebre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadhouseBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadhouseBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadhousebre/RoadhouseBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadhousebre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadhouseBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
